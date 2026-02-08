ce9da889c344acd25bf8f3e6cb6b19cee94d1461
GitHubAbhi1406 <mukherjee.abhijit1921@outlook.com>
2026-02-08 21:31:25 +0530

    Updated getAllProductsTest with new test method

--- src/test/java/com/DummyJSON/API/Tests/getAllProductsTest.java
+++ src/test/java/com/DummyJSON/API/Tests/getAllProductsTest.java
@@ -6,7 +6,10 @@ import org.testng.annotations.Test;
 import com.DummyJSON.API.ApiUtils.ApiUtils;
 import com.DummyJSON.API.baseFile.BaseTest;
 import com.DummyJSON.API.endpoints.Routes;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import java.util.List;
+import java.util.Map;
 
 public class getAllProductsTest extends BaseTest{
 	
@@ -25,5 +28,25 @@ public class getAllProductsTest extends BaseTest{
 	@Test(priority = 2)
 	public void checkStatusCode() {
 		Assert.assertEquals(res.getStatusCode(), 200);
+	}
+	
+	@Test(priority = 3)
+	public void extractResponseData() {
+		JsonPath jsonPath = res.jsonPath();
+		
+		List<Map<String, Object>> products_list = jsonPath.getList("products.findAll {it.category == 'beauty'}");
+		List<Map<String, Object>> products_stock_info = jsonPath.getList("products.findAll {it.availabilityStatus == 'In Stock'}");
+		
+		System.out.println("List of producst size : "+products_list.size());
+		
+		for(Map<String, Object> product : products_list) {
+			System.out.println(product.get("title"));
+		}
+		
+		System.out.println("\n");
+		
+		for(Map<String, Object> product : products_stock_info) {
+			System.out.println(product.get("title"));
+		}
 	}
 }
