117547c707a2607098e01170fd72a8de40f86179
GitHubAbhi1406 <mukherjee.abhijit1921@outlook.com>
2026-02-09 01:08:49 +0530

    new test method added

--- src/test/java/com/DummyJSON/API/Tests/getSingleCartTest.java
+++ src/test/java/com/DummyJSON/API/Tests/getSingleCartTest.java
@@ -1,10 +1,12 @@
 package com.DummyJSON.API.Tests;
 
+import java.util.List;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import com.DummyJSON.API.ApiUtils.ApiUtils;
 import com.DummyJSON.API.baseFile.BaseTest;
 import com.DummyJSON.API.endpoints.Routes;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class getSingleCartTest extends BaseTest{
@@ -17,9 +19,18 @@ public class getSingleCartTest extends BaseTest{
 		
 	}
 	
-	@Test
+	@Test(priority = 1)
 	public void printResponse() {
 		System.out.println("Response");
 		res.then().log().all();
+	}
+	
+	@Test(priority = 2)
+	public void extractTtile() {
+		JsonPath json = res.jsonPath();
+		
+		List<String> products_list = json.getList("products.title", String.class);
+		
+		System.out.println(products_list);
 	}
 }
